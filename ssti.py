886f900dd521e7d6884b079d2b931cb398fb4dfc
黃志仁 <tbsthitw@gmail.com>
2019-03-07 18:24:19 +0800

    modify ssti.py

--- ssti.py
+++ ssti.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template_string, config
+from flask import Flask, render_template_string, config, request
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name):
+@app.route('/')
+def index():
+	name=request.args.get('name')
 	template = '<h1>hello {}!<h1>'.format(name)
+	print(template)
 	return render_template_string(template)
 
 app.run()
